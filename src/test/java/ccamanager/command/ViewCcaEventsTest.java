8293d55233e86838310200384d4b801cc51d07b2
yy43yy <yiyang15624@gmail.com>
2026-04-04 12:24:36 +0800

    Add test case for viewCcaEvent

--- src/test/java/ccamanager/command/ViewCcaEventsTest.java
+++ src/test/java/ccamanager/command/ViewCcaEventsTest.java
@@ -12,12 +12,12 @@ import org.junit.jupiter.api.Test;
 import java.util.ArrayList;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-
-public class ViewMyEventsTest {
+public class ViewCcaEventsTest {
     private EventManager eventManager;
     private Resident resident1;
     private Resident resident2;
-    private Cca cca;
+    private Cca cca1;
+    private Cca cca2;
     private Ui ui;
 
     @BeforeEach
@@ -25,34 +25,30 @@ public class ViewMyEventsTest {
         eventManager = new EventManager();
         resident1 = new Resident("John Doe", "A1234567X");
         resident2 = new Resident("Jane Tan", "A7654321Y");
-        cca = new Cca("Basketball",CcaLevel.HIGH);
+        cca1 = new Cca("Basketball",CcaLevel.HIGH);
+        cca2 = new Cca("Football",CcaLevel.HIGH);
         ui = new Ui();
-
     }
 
     @Test
-    public void viewMyEvents_noEvents() {
-        ArrayList<Event> result = eventManager.viewMyEvents("A1234567X");
+    public void viewCcaEvents_noEvents() {
+        ArrayList<Event> result = eventManager.viewCcaEvents("Football");
         assertEquals(0, result.size());
     }
 
     @Test
-    public void viewMyEvents_oneMatchingEvent() throws EventNotFoundException {
-        eventManager.addEvent("Training",cca,"2026-04-03");
-        eventManager.addResidentToEvent("Training",cca,resident1);
-        ArrayList<Event> result = eventManager.viewMyEvents("A1234567X");
+    public void viewCcaEvents_oneMatchingEvent() throws EventNotFoundException {
+        eventManager.addEvent("Training",cca2,"2026-04-03");
+        ArrayList<Event> result = eventManager.viewCcaEvents("Football");
         assertEquals(1, result.size());
         assertEquals("Training", result.get(0).getEventName());
     }
     @Test
-    public void viewMyEvents_multipleMatchingEvent() throws EventNotFoundException {
-        eventManager.addEvent("Training",cca,"2026-04-03");
-        eventManager.addEvent("Game",cca,"2026-04-04");
-        eventManager.addEvent("Team Bonding",cca,"2026-04-04");
-        eventManager.addResidentToEvent("Training",cca,resident1);
-        eventManager.addResidentToEvent("Game",cca,resident1);
-        eventManager.addResidentToEvent("Team Bonding",cca,resident1);
-        ArrayList<Event> result = eventManager.viewMyEvents("A1234567X");
+    public void viewCcaEvents_multipleMatchingEvent() throws EventNotFoundException {
+        eventManager.addEvent("Training",cca1,"2026-04-03");
+        eventManager.addEvent("Game",cca1,"2026-04-04");
+        eventManager.addEvent("Team Bonding",cca1,"2026-04-04");
+        ArrayList<Event> result = eventManager.viewCcaEvents("Basketball");
         assertEquals(3, result.size());
         assertEquals("Training", result.get(0).getEventName());
         assertEquals("Game", result.get(1).getEventName());
