946af9f908c4d3d8772f8fbb1d4c588708a2ff4c
yy43yy <yiyang15624@gmail.com>
2026-04-13 21:14:45 +0800

    Fixed #98

--- src/main/java/ccamanager/parser/Parser.java
+++ src/main/java/ccamanager/parser/Parser.java
@@ -161,7 +161,7 @@ public class Parser {
             return new AddResidentToCcaCommand(args[0], args[1], args[2]);
 
         case "add-exco-to-cca":
-            if (args.length < 2) {
+            if (args.length < 3) {
                 return new UnknownCommand("Usage: add-exco-to-cca <matric number>; <cca name>");
             }
             if (args[0].isBlank()) {
